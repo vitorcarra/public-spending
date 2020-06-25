53d687ab79d615c9589c4b18806d3d6c545e9685
Vitor Carra <vitor.carra@gmail.com>
2020-06-25 00:46:46 -0300

    changing the cluster defition format

--- infrastructure/ecs/main.tf
+++ infrastructure/ecs/main.tf
@@ -6,8 +6,32 @@ resource "aws_ecs_cluster" "airflow-celery1" {
 
 resource "aws_ecs_task_definition" "webserver" {
   family                = "service"
-  container_definitions = file("${path.module}/task-definitions/webserver.json")
+  #container_definitions = file("${path.module}/task-definitions/webserver.json")
   requires_compatibilities = ["FARGATE"]
+
+  container_definitions = <<TASK_DEFINITION
+  [
+    {
+        "name": "webserver",
+        "image": "teste",
+        "essential": true,
+        "command": ["webserver"],
+        "portMappings": [
+            {
+                "containerPort": 8080,
+                "hostPort": 8080
+            }
+        ],
+        "environment": [
+            { "name": "POSTGRES_USER", "value": "${var.postgres_user}"},
+            { "name": "POSTGRES_PORT", "value": "${var.postgres_port}"},
+            { "name": "POSTGRES_HOST", "value": "${var.postgres_host}"},
+            { "name": "POSTGRES_DB", "value": "${var.postgres_db}"},
+            { "name": "POSTGRES_PASSWORD", "value": "${var.postgres_password}"}
+        ]
+    }
+  ]
+  TASK_DEFINITION
 
   task_role_arn = var.role_ecs_arn
   execution_role_arn = var.role_ecs_arn
